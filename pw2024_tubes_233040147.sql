7976d47800c3af1a900bc445e49bf521befcf9d5
roymss <145090061+roymss@users.noreply.github.com>
2024-06-10 01:09:42 +0700

    Add files via upload

---
+++ pw2024_tubes_233040147.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 09, 2024 at 01:03 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pw2024_tubes_233040147`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accs`
+--
+
+CREATE TABLE `accs` (
+  `id` int NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `tipe` varchar(50) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `harga` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `accs`
+--
+
+INSERT INTO `accs` (`id`, `gambar`, `tipe`, `judul`, `harga`) VALUES
+(1, 'produk.jpg', 'Accessesoris', 'Phantom GT2 ELITE', '$350.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int NOT NULL,
+  `gambar_admin` varchar(255) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `no_telpon` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `gambar_admin`, `nama_lengkap`, `username`, `email`, `alamat`, `no_telpon`) VALUES
+(1, 'bg1.jpg', 'Rafi Adyatma Sukanto', 'rafi88', 'rafi88@gmail.com', 'Jalan Geger Kalong no 190', '08215835132');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shoes`
+--
+
+CREATE TABLE `shoes` (
+  `id` int NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `tipe` varchar(50) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `harga` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `shoes`
+--
+
+INSERT INTO `shoes` (`id`, `gambar`, `tipe`, `judul`, `harga`) VALUES
+(1, 'produk.jpg', 'Phantom GT2 ELITE', 'Phantom GT2 ELITE', '$350.07'),
+(7, 'bg-2.jpg', 'asddddddddddasd', 'asddddddd', 'asddddddd'),
+(8, '6665a73b4305a.jpeg', 'zzzzzzzzzz', 'zzzzzzzzz', 'zzzzz'),
+(9, '6665a79d738ea.png', 'Budak', 'Rifan J2 Prime', '$25');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accs`
+--
+ALTER TABLE `accs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `shoes`
+--
+ALTER TABLE `shoes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accs`
+--
+ALTER TABLE `accs`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `shoes`
+--
+ALTER TABLE `shoes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
